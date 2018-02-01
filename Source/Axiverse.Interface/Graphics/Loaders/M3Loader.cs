d3c2f87265fde287e34e7bd9ad060b690f064e2c
Aaron Sun <axiverse@gmail.com>
2018-02-01 15:24:24 -0800

    Simpler entity system, separate graphics project.

---
+++ Source/Axiverse.Interface/Graphics/Loaders/M3Loader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axiverse.Interface.Graphics.Loaders
+{
+    // https://github.com/flo/m3addon/blob/master/structures.xml
+    // Development usage of SC2 Assets
+    class M3Loader
+    {
+    }
+}
---
+++ Source/Axiverse.Simulation/SimpleEntity/Entity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axiverse.Simulation.SimpleEntity
+{
+    /// <summary>
+    /// Represents an entity.
+    /// </summary>
+    public class Entity
+    {
+        /// <summary>
+        /// Gets the identifier for this entity.
+        /// </summary>
+        public Guid Identifier { get; }
+
+        /// <summary>
+        /// Gets or sets the name for this entity.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the collection of components in this entity.
+        /// </summary>
+        public ComponentCollection Components { get; }
+
+        /// <summary>
+        /// Constructs an entity with a new identifier.
+        /// </summary>
+        public Entity() : this(Guid.NewGuid())
+        {
+        }
+
+        /// <summary>
+        /// Constructs an entity with the given identifier.
+        /// </summary>
+        /// <param name="identifier"></param>
+        public Entity(Guid identifier)
+        {
+            Identifier =identifier;
+            Components = new ComponentCollection();
+        }
+
+        /// <summary>
+        /// Sets the component bound to the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Get<T>() where T : Component
+        {
+            return Components[typeof(T)] as T;
+        }
+
+        /// <summary>
+        /// Gets a component bound to the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="component"></param>
+        public void Set<T>(T component) where T : Component
+        {
+            Components[typeof(T)] = component;
+        }
+    }
+}
