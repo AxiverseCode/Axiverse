763e1f8905c3ea6391bbd70fbe0040223f8e4b9b
Nacho Cortizo <nachocpol@gmail.com>
2018-02-03 12:57:49 +0000

    Added initial buffer class.

--- Source/Axiverse.Interface/Engine/Rendering/Buffer.cs
+++ Source/Axiverse.Interface/Engine/Rendering/Buffer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using SharpDX;
 using SharpDX.Direct3D12;
 
 namespace Axiverse.Interface.Engine.Rendering
@@ -11,22 +13,53 @@ namespace Axiverse.Interface.Engine.Rendering
     {
         private Resource mUploadHeap;
         private Resource mGPUHeap;
-        public GpuDescriptorHandle View;
 
-        void InitAsIndexBuffer(ref Device device)
+        private IndexBufferView mIndexBufferView;
+        public VertexBufferView mVertexBufferView;
+
+        void InitAsIndexBuffer(ref Device device,IntPtr data,int dataSize)
         {
+            // Upload Heap, CPU can write to it. 
             mUploadHeap = device.CreateCommittedResource
             (
-                    new HeapProperties(HeapType.Upload),
-                    HeapFlags.None,
-                    ResourceDescription.Buffer(100),
-                    ResourceStates.CopySource
+                new HeapProperties(HeapType.Upload),
+                HeapFlags.None,
+                ResourceDescription.Buffer(dataSize),
+                ResourceStates.GenericRead
             );
+
+            // Copy data
+            Range mapRange = new Range
+            {
+                Begin = 0,
+                End = 0
+            };
+            IntPtr pData = mUploadHeap.Map(0,mapRange);
+            Utilities.CopyMemory(pData, data, dataSize);
+            mUploadHeap.Unmap(0,null);
+
+            // Create a view
+            mIndexBufferView = new IndexBufferView
+            {
+                Format = SharpDX.DXGI.Format.Unknown,
+                SizeInBytes = dataSize,
+                BufferLocation = mUploadHeap.GPUVirtualAddress
+            };
         }
            
         void InitAsVertexBuffer()
         {
 
+        }
+
+        IndexBufferView AsIndexBuffer()
+        {
+            return mIndexBufferView;
+        }
+
+        VertexBufferView AsVertexBuffer()
+        {
+            return mVertexBufferView;
         }
     }
 }
