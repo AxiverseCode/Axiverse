6b11cbe1b184be858cfa75bb364ea6e5d99bca30
Aaron Sun <axiverse@gmail.com>
2018-03-27 03:30:25 -0700

    Generated services and web

---
+++ Source/Axiverse.Services.Proto/Generate.bat
@@ -0,0 +1,12 @@
+..\packages\Grpc.Tools.1.10.0\tools\windows_x86\protoc.exe ^
+	-I..\..\Proto ^
+	--csharp_out . ^
+	--grpc_out . ^
+	..\..\Proto\Admin\AliasService.proto ^
+	..\..\Proto\Admin\IdentityService.proto ^
+	..\..\Proto\AssetService.proto ^
+	..\..\Proto\EntityService.proto ^
+	..\..\Proto\MarketService.proto ^
+	..\..\Proto\ModelService.proto ^
+	..\..\Proto\ResourceService.proto ^
+	--plugin=protoc-gen-grpc=..\packages\Grpc.Tools.1.10.0\tools\windows_x86\grpc_csharp_plugin.exe
