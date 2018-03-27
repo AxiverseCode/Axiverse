a5d471e1682264fda7a877c10d16c6f0cf51c0b5
Aaron Sun <axiverse@gmail.com>
2018-03-27 01:04:41 -0700

    Math test project.

---
+++ Tests/Axiverse.Tests.Mathematics/Spatial/OctreeTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Axiverse.Tests.Mathematics.Spatial
+{
+    [TestClass]
+    public class OctreeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tests/Axiverse.Tests.Mathematics/Vector3Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Axiverse.Tests.Mathematics
+{
+    [TestClass]
+    public class Vector3Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
