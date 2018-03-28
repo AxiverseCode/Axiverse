462a9f84c5fd860bc3dbf821b3d9a754e4ac42b7
Aaron Sun <axiverse@gmail.com>
2018-03-27 23:54:26 -0700

    IdentityService stubs.

---
+++ Source/Axiverse.Services.IdentityService/IdentityModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+
+namespace Axiverse.Services.IdentityService
+{
+    class IdentityModule
+    {
+        public void Install()
+        {
+            var port = 9090;
+
+            Server server = new Server
+            {
+                Services = { Proto.IdentityService.BindService(new IdentityServiceImpl()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("RouteGuide server listening on port " + port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+
+        }
+    }
+}
---
+++ Source/Axiverse.Services.IdentityService/IdentityServiceImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Axiverse.Services.Proto;
+using Grpc.Core;
+
+namespace Axiverse.Services.IdentityService
+{
+    public class IdentityServiceImpl : Proto.IdentityService.IdentityServiceBase
+    {
+        public override Task<ValidateIdentityResponse> ValidateIdentity(ValidateIdentityRequest request, ServerCallContext context)
+        {
+            var response = new ValidateIdentityResponse();
+
+
+            return Task.FromResult(response);
+        }
+    }
+}
