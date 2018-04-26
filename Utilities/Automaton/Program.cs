c8f1ac4992fcebb57c44eebe80bb4c8d12b4fb33
Aaron Sun <axiverse@gmail.com>
2018-04-26 14:28:36 -0700

    Automation service host project

---
+++ Utilities/Automaton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/framework/deployment/best-practices-for-assembly-loading
+            // https://stackoverflow.com/questions/15113352/find-out-dependencies-of-all-dlls
+        }
+    }
+}
