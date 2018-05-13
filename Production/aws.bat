ab1bdc2d20979943099aa3a8f36bd6a16ece865f
Aaron Sun <axiverse@gmail.com>
2018-05-12 20:09:18 -0700

    Aws build scripts

--- Production/aws.bat
+++ Production/aws.bat
@@ -4,16 +4,20 @@
 # aws ecr get-login --no-include-email --region us-east-1
 # docker login
 
-docker build -t production_build -f ..\Production\build.dockerfile .
+docker build -t production_build -f .\build.dockerfile ..\Source
 
-docker build -t calibrate-identity-dev -f ..\Production\identity-service.dockerfile .
+docker build -t calibrate-identity-dev -f .\service-identity.dockerfile .
 docker tag calibrate-identity-dev:latest 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-identity-dev:latest
 docker push 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-identity-dev:latest
 
-docker build -t calibrate-identity-dev -f ..\Production\chat-service.dockerfile .
+docker build -t calibrate-chat-dev -f .\service-chat.dockerfile .
 docker tag calibrate-chat-dev:latest 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-chat-dev:latest
 docker push 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-chat-dev:latest
 
-docker build -t calibrate-identity-dev -f ..\Production\entity-service.dockerfile .
+docker build -t calibrate-entity-dev -f .\service-entity.dockerfile .
 docker tag calibrate-entity-dev:latest 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-entity-dev:latest
 docker push 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-entity-dev:latest
+
+docker build -t calibrate-web-dev -f .\web.dockerfile ..\Web
+docker tag calibrate-web-dev:latest 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-web-dev:latest
+docker push 214054665271.dkr.ecr.us-east-1.amazonaws.com/calibrate-web-dev:latest
