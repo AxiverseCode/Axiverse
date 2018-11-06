6c3150a7d7515dbd12ee0569a32dbdbd0cca6e28
Aaron Sun <axiverse@gmail.com>
2018-11-05 22:17:56 -0800

    Starting vm implementation.

---
+++ Source/Libraries/Axiverse.Computing/VirtualMachine/Machine.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.BitConverter;
+namespace Axiverse.Computing.VirtualMachine
+{
+    public class Machine
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Moves forward
+        /// </remarks>
+        public int InstructionPointer { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Moves backward.
+        /// </remarks>
+        public int StackPointer { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        public int FramePointer { get; set; }
+
+        public byte[] Memory { get; set; } = new byte[1024];
+
+        // 32 bit?
+        public void Go()
+        {
+            // http://web.cse.ohio-state.edu/~reeves.92/CSE2421au12/SlidesDay43.pdf
+            Opcode opcode;
+            while ((opcode = (Opcode)Memory[InstructionPointer++]) != Opcode.Halt)
+            {
+                switch (opcode)
+                {
+                    case Opcode.Add:
+                        {
+                            // Pop 2 from the stack
+                            var a = ToInt32(Memory, StackPointer);
+                            StackPointer += sizeof(Int32);
+                            var b = ToInt32(Memory, StackPointer);
+                            StackPointer += sizeof(Int32);
+
+                            var c = a + b;
+                            Buffer.BlockCopy(GetBytes(c), 0, Memory, StackPointer, sizeof(Int32));
+                            StackPointer -= sizeof(Int32);
+
+                            break;
+                        }
+                    case Opcode.Call:
+                        {
+                            // Allocate return space
+                            // Push parameters
+                            // 'Call'
+                            // - Allocates locals
+                            // - Writes return address (IP)
+                            // - Sets frame pointer
+                            // - Write previous frame pointer
+                            break;
+                        }
+                    case Opcode.Return:
+                        {
+                            // 
+                            break;
+                        }
+                    case Opcode.Const:
+                        {
+                            // read 32 bit.
+                            var v = ToInt32(Memory, InstructionPointer);
+                            InstructionPointer += sizeof(Int32);
+
+                            // push onto stack.
+                            Buffer.BlockCopy(GetBytes(v), 0, Memory, StackPointer, sizeof(Int32));
+                            StackPointer -= sizeof(Int32);
+
+                            break;
+                        }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Source/Libraries/Axiverse.Computing/VirtualMachine/Opcode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axiverse.Computing.VirtualMachine
+{
+    /// <summary>
+    /// Virtual machine operation codes.
+    /// </summary>
+    public enum Opcode : byte
+    {
+        Nop = 0x00,
+
+        /// <summary>
+        /// 
+        /// Takes argument count.
+        /// </summary>
+        Call,
+
+        Return,
+        Halt,
+
+        /// <summary>
+        /// 
+        /// Adds two 32 bit numbers together from the stack.
+        /// </summary>
+        Add,
+
+        /// <summary>
+        /// Load variable relative to fp.
+        /// </summary>
+        Load,
+
+        /// <summary>
+        /// Load variable from global memory.
+        /// </summary>
+        GlobalLoad,
+
+        /// <summary>
+        /// Store variable relative to fp.
+        /// </summary>
+        Store,
+
+        /// <summary>
+        /// Store variable into global memory from stack.
+        /// </summary>
+        GlobalStore,
+
+        /// <summary>
+        /// Load a constant variable.
+        /// </summary>
+        Const,
+
+        Print,
+    }
+}
