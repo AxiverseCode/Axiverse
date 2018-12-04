112c15d703bf1c8a24e730fb9f475466fb8995a5
Aaron Sun <axiverse@gmail.com>
2018-12-04 01:17:51 -0800

    Missing files

---
+++ Source/Libraries/Axiverse.Mathematics/Graphs/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axiverse.Mathematics.Graphs
+{
+    public class Edge<T>
+    {
+        public Node<T> Former { get; set; }
+        public Node<T> Latter { get; set; }
+    }
+}
---
+++ Source/Libraries/Axiverse.Mathematics/Graphs/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axiverse.Mathematics.Graphs
+{
+    public class Node<T>
+    {
+        List<T> Outgoing { get; } = new List<T>();
+        List<T> Incoming { get; } = new List<T>();
+    }
+}
